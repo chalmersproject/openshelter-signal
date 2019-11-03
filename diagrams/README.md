fec9b7b29d1552a91ba6318ee925b37167245c0c
zacharydonsky <zachary.donsky@gmail.com>
2019-11-03 02:44:52 -0500

    only posts json schema once with dummy data
    first working commit that just tests the firebase library by making a json object and sending it to firebase, and then reading values back from firebase.

---
+++ diagrams/README.md
@@ -0,0 +1,10 @@
+# Diagrams
+Diagram ```.drawio``` files can be viewed with Draw.io.
+
+[Use the Web Interface for Draw.io](https://draw.io) (no sign in required)
+    + Click ^^that link 
+    + click "Decide later" near bottom
+    + click ```File/Open from/Device```
+    + open the ```*.drawio``` file
+
+[Install the Desktop version for Draw.io](https://github.com/jgraph/drawio-desktop/releases) (free/open source && linux/macos/windows friendly)
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,54 @@
 #include <Arduino.h>
+
+//display
 #include <SPI.h>
 #include <Adafruit_ILI9341.h>
 #include <Adafruit_GFX.h>
 
+//internet
+#include "FirebaseESP8266.h"
+#include <ESP8266WiFi.h>
+
+//
+// !!!!!!!!!!! THIS FILE IS .gitignore'd !!!!!!!!!!!
+// it includes API keys SSIDs, and passwords
+#include "credentials.h"
+
+//Define Firebase Data object
+FirebaseData firebaseData;
+
+//functions
+#include "connect.h"
+
+
+//available space
+int firecode_capacity = 100;
+int bed_capacity      = 43;
+//------------------------//
+int firecode_occupancy = 42;
+int bed_occupancy = 42;
+
+//meal_status (meal, no_meal, snacks)
+String meal_status = "meal";
+
+//Shelter Properties
+bool adult_only = false;
+bool youth_only = false;
+bool family_only = false;
+bool male_only = false;
+bool female_only = false;
+bool lgbtq_only = false;
+bool all_allowed = true;
+
+#include "json_test.h"
+
 void setup() {
-  // put your setup code here, to run once:
+  connect_Serial();
+  connect_Wifi();
+  connect_Firebase();
+
+  json_test();
+
 }
 
 void loop() {
