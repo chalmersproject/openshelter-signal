e54a1177c17f880efdb693a2dcd597560e90cce7
Zachary Donsky <zach@chalmerscards.com>
2020-02-23 21:07:28 -0500

    fixed stack trace. FirebaseJson object was running out of memory because I put too many properties on it. Instead I created another FirebaseJson object called Shelter_Contact which will hold friendly name, phone number, and address

--- include/firebase_json.h
+++ include/firebase_json.h
@@ -16,14 +16,17 @@ void set_local_json()
   // Service_Status.addJson("Firecode_Space", &Firecode_Space).addJson("Bed_Space", &Bed_Space);
 
   FirebaseJson Shelter_Properties;
-  // Shelter_Properties.addDouble("latitude", shelter_latitude).addDouble("longitude", shelter_longitude).addString("phone_number", phone_number).addString("friendly_name", friendly_name);
+  Shelter_Properties.addDouble("latitude", shelter_latitude).addDouble("longitude", shelter_longitude).addString("phone_number", phone_number).addString("friendly_name", friendly_name);
 
-  Shelter_Properties.addDouble("latitude", shelter_latitude).addDouble("longitude", shelter_longitude);
+  FirebaseJson Shelter_Contact;
+  Shelter_Contact.addString("phone_number", phone_number).addString("friendly_name", friendly_name);
+
+  // Shelter_Properties.addDouble("latitude", shelter_latitude).addDouble("longitude", shelter_longitude);
 
   FirebaseJson Client_Properties;
   Client_Properties.addBool("adult_only",adult_only).addBool("youth_only",youth_only).addBool("family_only",family_only).addBool("male_only", male_only).addBool("female_only",female_only).addBool("lgbtq_only",lgbtq_only).addBool("all_allowed",all_allowed);
 
-  json.addJson("Service_Status", &Service_Status).addJson("Shelter_Properties", &Shelter_Properties).addJson("Client_Properties", &Client_Properties);
+  json.addJson("Service_Status", &Service_Status).addJson("Shelter_Properties", &Shelter_Properties).addJson("Shelter_Contact", &Shelter_Contact).addJson("Client_Properties", &Client_Properties);
 }
 
 void write_local_to_remote()
