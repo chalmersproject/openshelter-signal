817633d3f3addd3372f0bae03b0d14b7f7865d9b
Zachary Donsky <zachary.donsky@achievers.com>
2020-04-03 23:13:45 -0400

    added friendly_address and pet_friendly fields to firebase json; added shelter template

---
+++ include/Shelters/template.h
@@ -0,0 +1,34 @@
+//////////////////////////////////////////////////////////
+//                Shelter Properties                    //
+//////////////////////////////////////////////////////////
+#define shelter_latitude 00.0000000
+#define shelter_longitude -00.0000000
+String phone_number = "(416) 555-5555";
+String shelter_name = "best_shelter_in_the_world";
+String friendly_name = "The best shelter in the world";
+String friendly_address = "555 Somewherin Toronto";
+
+//available space
+int firecode_occupancy = 0;
+int firecode_capacity = 100;
+//------------------------//
+int bed_occupancy = 0;
+int bed_capacity  = 60;
+
+//Client Properties
+bool adult_only = false;
+bool youth_only = false;
+bool family_only = false;
+bool male_only = false;
+bool female_only = false;
+bool lgbtq_only = false;
+bool all_allowed = false;
+bool pet_friendly = false;
+
+//meal_status (meal, no_meal, snacks)
+String meal_status = "meal";
+
+//------------------------//
+// creates path variables for firebase to use
+String path = "/Shelters/" + shelter_name;
+String path_firecode_occupancy = "/Shelters/" + shelter_name + "/Service_Status/Firecode_Space/Firecode_Occupancy";
--- include/firebase_json.h
+++ include/firebase_json.h
@@ -24,7 +24,7 @@ void set_local_json()
   // Service_Status.addJson("Firecode_Space", &Firecode_Space).addJson("Bed_Space", &Bed_Space);
 
   FirebaseJson Shelter_Properties;
-  Shelter_Properties.addDouble("latitude", shelter_latitude).addDouble("longitude", shelter_longitude).addString("phone_number", phone_number).addString("friendly_name", friendly_name);
+  Shelter_Properties.addDouble("latitude", shelter_latitude).addDouble("longitude", shelter_longitude).addString("phone_number", phone_number).addString("friendly_name", friendly_name).addString("friendly_address",friendly_address);
 
   FirebaseJson Shelter_Contact;
   Shelter_Contact.addString("phone_number", phone_number).addString("friendly_name", friendly_name);
@@ -32,7 +32,7 @@ void set_local_json()
   // Shelter_Properties.addDouble("latitude", shelter_latitude).addDouble("longitude", shelter_longitude);
 
   FirebaseJson Client_Properties;
-  Client_Properties.addBool("adult_only",adult_only).addBool("youth_only",youth_only).addBool("family_only",family_only).addBool("male_only", male_only).addBool("female_only",female_only).addBool("lgbtq_only",lgbtq_only).addBool("all_allowed",all_allowed);
+  Client_Properties.addBool("adult_only",adult_only).addBool("youth_only",youth_only).addBool("family_only",family_only).addBool("male_only", male_only).addBool("female_only",female_only).addBool("lgbtq_only",lgbtq_only).addBool("pet_friendly", pet_friendly).addBool("all_allowed",all_allowed);
 
   json.addJson("Service_Status", &Service_Status).addJson("Shelter_Properties", &Shelter_Properties).addJson("Shelter_Contact", &Shelter_Contact).addJson("Client_Properties", &Client_Properties);
 }
