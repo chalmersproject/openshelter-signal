83dfc4b51e2e7c8ed72462c2c5455dfc1c869505
Zachary Donsky <zach@chalmerscards.com>
2020-09-28 01:45:07 -0400

    added intro to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,13 @@
-//when encoder clk triggers
-// counter + or -
-// if counter % 2 == 0          //the rotary encoder counts twice per "bump"
-//update occupancy(counter/2) //so I just count 2 jumps as 1
-//update tft(occupancy)
-//update led(occupancy)
+//////////////////////////////////////////////////////////
+//            CHALMERS SIGNAL OCCUPANCY                 //
+//////////////////////////////////////////////////////////
+/*
+The Chalmers Signal Occuapncy Device is a client tally counter
+for shelter reception staff. 
 
-//in main loop
-//if occupancy is different from last_occupancy
-// change_to_push=true
-// firebase_upload_countdown_start=now
-// if (now - firebase_upload_countdown_start > 4000 && change_to_push == true)
-// upload to firebase
-// firebase_upload_countdown_start=now
-// change_to_push = false
+Written an Maintained by : Zachary Donsky zach@chalmerscards.com
+*/
+
 
 //////////////////////////////////////////////////////////
 //                     Includes                         //
@@ -35,6 +30,10 @@
 //////////////////////////////////////////////////////////
 //                    Toggles                           //
 //////////////////////////////////////////////////////////
+ 
+// The properties fo the shelter this chalmers signal is for. Defines occupancy, capacity, etc.
+#include "Shelters/housing_first_strachan_house.h"
+
 // some chalmers signals have red-pcb 1.44" displays from creatron
 // others use the cheaper blue-pcb 1.44" displays from aliexpress
 static int display_color = 1; //(blue_pcb = 1; red_pcb = 2)
@@ -46,11 +45,8 @@ static bool enable_internet = false;
 static bool has_button = false;
 
 //////////////////////////////////////////////////////////
-//             Globals and Utilities                    //
+//                    Globals                           //
 //////////////////////////////////////////////////////////
-
-// Which shelter this chalmers signal is for. Defines occupancy, capacity, etc.
-#include "Shelters/housing_first_strachan_house.h"
 
 //Rotary Encoder Global Variables
 #define inputCLK 4
