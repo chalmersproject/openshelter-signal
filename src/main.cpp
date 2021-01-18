7e2071d4496909f0b05dc5195939801c2e2a5ed7
chalmers <zach@chalmerscards.com>
2021-01-17 21:37:35 -0500

    wip; moved graphql code into main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,13 @@
 The Chalmers Signal Occuapncy Device is a client tally counter
 for shelter reception staff. 
 
-Written an Maintained by : Zachary Donsky zach@chalmerscards.com
+Written an Maintained by the Chalmers project info@chalmersproject.com
 F/OSS under M.I.T License
 */
 
-//////////////////////////////////////////////////////////
-//                     Includes                         //
-//////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+//                     Includes                                                          //
+///////////////////////////////////////////////////////////////////////////////////////////
 #include <Arduino.h>
 
 //display
@@ -20,22 +20,27 @@ F/OSS under M.I.T License
 #include <Adafruit_GFX.h>
 #include <RotaryEncoder.h>
 
-//internet
-#include "FirebaseESP8266.h"
+//WiFi and HTTPS requests
+#include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h> // create TLS connection
 
 //LED
 #include <FastLED.h>
 
-//////////////////////////////////////////////////////////
-//                    Toggles                           //
-//////////////////////////////////////////////////////////
+//
+// JSON Support
+//
+#include <ArduinoJson.h>
 
-// The properties fo the shelter this chalmers signal is for. Defines occupancy, capacity, etc.
-#include "Shelters/st_felix_augusta.h"
-// #include "Shelters/housing_first_strachan_house.h"
-// #include "Shelters/fred_victor_strachan.h"
+//
+// API secret and shelter ID
+//
+#include <shelter_secrets.h>
 
+///////////////////////////////////////////////////////////////////////////////////////////
+//                    Toggles                                                            //
+///////////////////////////////////////////////////////////////////////////////////////////
 
 // some chalmers signals have red-pcb 1.44" displays from creatron
 // others use the cheaper blue-pcb 1.44" displays from aliexpress
@@ -47,16 +52,26 @@ static bool enable_internet = true;
 // earlier versions of chalmers signals don't have their button attached to the ESP. It's useful to be able to quickly turn off all features of the chalmers signal that use this button.
 static bool has_button = false;
 
-//////////////////////////////////////////////////////////
-//                    Globals                           //
-//////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+//                    Globals                                                            //
+///////////////////////////////////////////////////////////////////////////////////////////
 
-//Rotary Encoder Global Variables
-#define inputCLK 4 // pin 
+//
+// Measured occupancy value from the chalmers signal
+//
+int _MEASUREMENT = 15;
+int capacity = 90;
+
+//
+// Rotary Encoder Global Variables
+//
+#define inputCLK 4 // pin
 #define inputDT 5
 RotaryEncoder encoder(5, 4);
 
-//TFT Globals
+//
+// TFT Globals
+//
 // displays from creatron and aliexpress require different coordinates
 #define y1 ((display_color == 1) ? 18 : 42) //top left of top digit
 #define y2 ((display_color == 1) ? 48 : 67) //top left of divider line
@@ -74,9 +89,11 @@ RotaryEncoder encoder(5, 4);
 #define WHITE 0xFFFF
 //instatiate TFT!
 TFT_ILI9163C tft = TFT_ILI9163C(__CS, __DC);
-int last_occupancy; //used to detect when occupancy has grown by one digit ( e.g. 9 -> 10 ) and occupancy has to be wiped from the LCD
+int last_MEASUREMENT; //used to detect when occupancy has grown by one digit ( e.g. 9 -> 10 ) and occupancy has to be wiped from the LCD
 
+//
 // LED Globals
+//
 int hue = 0;
 uint32 led_last;
 #define NUM_LEDS 8
@@ -84,25 +101,113 @@ uint32 led_last;
 CRGB leds[NUM_LEDS];
 int led_brightness = 64;
 
-//Wifi Globals
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-// !!!!!!!!!!! THIS FILE IS .gitignore'd !!!!!!!!!!!
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-// it includes API keys SSIDs, and passwords
-// You need to fill it in with your own credentials
-// before this program will work.
-#include "credentials.h"
+///////////////////////////////////////////////////////////////////////////////////////////
+//                    FUNCTIONS                                                          //
+///////////////////////////////////////////////////////////////////////////////////////////
 
-//Firebase Globals
-FirebaseData firebaseData;
-FirebaseJson json;
-FirebaseJsonObject jsonParseResult;
-#include "firebase_json.h" // for pushing new shelter entry if firebase doesn't have this shelter in it already
+WiFiClientSecure client;
+void initWifi()
+{
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(_WIFI_SSID, _WIFI_PWD);
 
-//////////////////////////////////////////////////////////
-//             Script Starts Here                       //
-//////////////////////////////////////////////////////////
+  Serial.println("Connecting..");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.println(".");
+  }
+  Serial.printf("SSID: %s\nIP: %s\n", _WIFI_SSID, WiFi.localIP().toString().c_str());
+}
 
+///////////////////////////////////////////////////////////////////////////////////////////
+//                    QUERY                                                              //
+///////////////////////////////////////////////////////////////////////////////////////////
+
+//
+// REQUEST FORMAT:
+//
+
+/*
+{
+  query: "...the raw query text...",
+  variables: {
+    signalId: "...",
+    signalSecret: "...",
+    _MEASUREMENT: 10
+  }
+}
+
+reqJson = {
+  query: "...",
+  variables: varJson
+}
+*/
+
+// #define SYNCPRINT_SIZE 256
+#define REQBUFF_SIZE 256
+#define VARBUFF_SIZE 256
+#define RESPBUFF_SIZE 2048
+
+const char *_API_HOST = "https://api.chalmersproject.com/graphql";
+// Attempting to do a multi-line variable declaration: HOWTO?
+const char *MUTATION = "           \
+mutation MeasureShelterOccupancy(  \
+  $signalId: ID!                   \
+  $signalSecret: String!           \
+  $measurement: Int!               \
+) {                                \
+  measureShelterOccupancy(         \
+    input: {                       \
+      signalId: $signalId          \
+      signalSecret: $signalSecret  \
+      measurement: $measurement    \
+    }                              \
+  ) {                              \
+    measurement {                  \
+      id                           \
+    }                              \
+  }                                \
+}";
+
+typedef struct graphqlQuery
+{
+  char req[REQBUFF_SIZE];
+  char var[VARBUFF_SIZE];
+  int status;
+  String resp;
+} GraphqlQuery;
+
+// HTTP POST to chalmersproject API
+void occupancy_request(WiFiClientSecure client, int _MEASUREMENT)
+{
+  // GraphqlQuery *graphql = (GraphqlQuery *)malloc(sizeof(GraphqlQuery));
+  HTTPClient http;
+  DynamicJsonDocument reqJson(1024);
+  DynamicJsonDocument varJson(1024);
+  varJson["signalId"] = SIGNAL_ID;
+  varJson["signalSecret"] = SIGNAL_SECRET;
+  varJson["measurement"] = _MEASUREMENT;
+
+  Serial.println("Sending HTTP POST");
+  http.begin(client, _API_HOST);
+  http.addHeader("Content-Type", "application/json");
+  reqJson["query"] = MUTATION;
+  reqJson["variables"] = varJson;
+
+  String request;
+  serializeJson(reqJson, request);
+  Serial.print("Request: ");
+  Serial.println(request);
+
+  http.POST(request);
+
+  Serial.print("Response: ");
+  Serial.println(http.getString());
+}
+///////////////////////////////////////////////////////////////////////////////////////////
+//                    Rotary Encoder Interrupt                                           //
+///////////////////////////////////////////////////////////////////////////////////////////
 // flag to mark when the dial has been moved. Calls the LEDs, LCD, and push_to_firebase functions
 bool change_to_push = false;
 
@@ -111,66 +216,46 @@ void ICACHE_RAM_ATTR encoder_change_trigger()
   encoder.tick();
   Serial.println("interrupt triggered!");
 }
+
+///////////////////////////////////////////////////////////////////////////////////////////
+//                    MAIN SCRIPT STARTS HERE                                            //
+///////////////////////////////////////////////////////////////////////////////////////////
+
 void setup()
 {
   Serial.begin(115200);
-  Serial.println("======================");
-  Serial.println("======================");
-  Serial.println("Start Chalmers Signal!");
-  Serial.println("======================");
-  Serial.println("======================");
-
   attachInterrupt(digitalPinToInterrupt(4), encoder_change_trigger, CHANGE);
   attachInterrupt(digitalPinToInterrupt(5), encoder_change_trigger, CHANGE);
 
   FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
   FastLED.setBrightness(led_brightness);
 
-  tft.begin();
-  tft.setRotation(2);
-  tft.setCursor(35, y1);
-  tft.setTextColor(WHITE, BLACK);
-  tft.setTextSize(5);
-  tft.println(occupancy);
-  tft.setCursor(8, y2);
-  tft.println("----");
-  tft.setCursor(35, y3);
-  tft.println(capacity);
-
-  hue = map(occupancy, 0, capacity, 171, 0);
+  hue = map(_MEASUREMENT, 0, capacity, 171, 0);
   CHSV color = CHSV(hue, 255, 255);
   fill_solid(leds, NUM_LEDS, color);
   FastLED.show();
 
-  if (enable_internet == true)
+  tft.begin();
+  tft.setRotation(2);
+  tft.setTextColor(WHITE, BLACK);
+  tft.setTextSize(3);
+  tft.println("CHALMERS");
+  tft.println("SIGNAL");
+
+  initWifi();
+  // HTTPClient http;
+  client.setInsecure();
+  if (WiFi.status() == WL_CONNECTED)
   {
-    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-    Serial.print("Connecting to Wi-Fi");
-    while (WiFi.status() != WL_CONNECTED)
-    {
-      Serial.print(".");
-      delay(300);
-    }
-    Serial.println();
-    Serial.print("Connected with IP: ");
-    Serial.println(WiFi.localIP());
-    Serial.println();
-    Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-    Firebase.reconnectWiFi(true);
-    Serial.println("Firebase Connnected");
-    //if shelter info does not already exist in firebase
-    //create a json object at shelter path and push to firebase
-    if (!Firebase.getJSON(firebaseData, path))
-    {
-      Serial.println("Shelter data does not exist in firebase!");
-      Serial.println("Creating Shelter data and pushing to firebase!");
-      set_local_json();
-      write_local_to_remote();
-    }
+    tft.clearScreen(BLACK);
+    tft.println("CONNECTED!");
+    delay(4000);
+    tft.clearScreen(BLACK);
   }
 }
 
 unsigned long now, last;
+
 void loop()
 {
   static int pos = 0;
@@ -183,60 +268,53 @@ void loop()
     Serial.println();
     if (pos > newPos)
     {
-      occupancy--;
+      _MEASUREMENT--;
     }
     else if (newPos > pos)
     {
-      occupancy++;
+      _MEASUREMENT++;
     }
-    Serial.print("Occupancy: ");
-    Serial.print(occupancy);
+    Serial.print("_MEASUREMENT: ");
+    Serial.print(_MEASUREMENT);
     Serial.println();
     pos = newPos;
 
-    //set barriers on occupancy
-    if (0 >= occupancy)
+    //set barriers on _MEASUREMENT
+    if (0 >= _MEASUREMENT)
     {
-      occupancy = 0;
+      _MEASUREMENT = 0;
     }
-    else if (occupancy >= capacity)
+    else if (_MEASUREMENT >= capacity)
     {
-      occupancy = capacity;
+      _MEASUREMENT = capacity;
     }
     tft.setCursor(35, y1);
-    // used to detect when occupancy has grown by one digit ( e.g. 10 -> 9 ) and occupancy has to be wiped from the LCD
-    if (occupancy == 9 && last_occupancy == 10 || occupancy == 99 && last_occupancy == 100)
+    // used to detect when _MEASUREMENT has grown by one digit ( e.g. 10 -> 9 ) and _MEASUREMENT has to be wiped from the LCD
+    if (_MEASUREMENT == 9 && last_MEASUREMENT == 10 || _MEASUREMENT == 99 && last_MEASUREMENT == 100)
     {
       tft.fillRect(35, y1, tft.width(), (y2 - 25), BLACK);
-      last_occupancy = occupancy;
+      last_MEASUREMENT = _MEASUREMENT;
     }
-    tft.println(occupancy);
+    tft.println(_MEASUREMENT);
     // update LEDs
-    hue = map(occupancy, 0, capacity, 171, 0);
+    hue = map(_MEASUREMENT, 0, capacity, 171, 0);
     CHSV color = CHSV(hue, 255, 255);
     fill_solid(leds, NUM_LEDS, color);
     FastLED.show();
     change_to_push = true;
     last = now;
-    last_occupancy = occupancy;
+    last_MEASUREMENT = _MEASUREMENT;
   }
 
-  // wait at least 3 seconds since last change before pushing to firebase
-  // if firebase gets updated too often it will cost a lot of money
+  //
+  // wait at least 3 seconds since last change before pushing to api.chalmers.project
+  //
   now = millis();
   if (now - last >= 3000 && change_to_push)
   {
     if (enable_internet == true)
     {
-      if (Firebase.setInt(firebaseData, path_firecode_occupancy, occupancy))
-      {
-        Serial.println("Updating Firebase!!");
-      }
-      else
-      {
-        Serial.println("REASON: " + firebaseData.errorReason());
-        delay(1000);
-      }
+      occupancy_request(client, _MEASUREMENT);
       change_to_push = false;
     }
   }
