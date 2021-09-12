1d24d48e432b25075ac072b5aa4667910be79f12
Zachary Donsky <zachary.donsky@achievers.com>
2021-09-11 20:53:42 -0400

    WifiManager now properly tries remembered wifi credentials and connects automatically if it has some that work

--- src/main.cpp
+++ src/main.cpp
@@ -67,7 +67,7 @@ int last_occupancy;
 //
 #define inputCLK 4 // pin
 #define inputDT 5
-    RotaryEncoder encoder(5, 4);
+RotaryEncoder encoder(5, 4);
 
 #include <tft_globals.h>
 
@@ -88,15 +88,15 @@ int led_brightness = 64;
 WiFiClientSecure client;
 void initWifi()
 {
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(_WIFI_SSID, _WIFI_PWD);
+  WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
+  // WiFi.begin(_WIFI_SSID, _WIFI_PWD);
 
   // https://github.com/tzapu/WiFiManager/blob/master/examples/Basic/Basic.ino
   // WiFiManager, Local intialization. Once its business is done, there is no need to keep it around  
   WiFiManager wm;
 
   bool res;
-  res = wm.autoConnect("Chalmers-Signal", "fart"); // password protected ap
+  res = wm.autoConnect("Chalmers-Signal");
   if (!res)
   {
     Serial.println("Failed to connect");
