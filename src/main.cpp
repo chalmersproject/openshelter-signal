7b48b2097afa80b574bc9ac8727ceee66ac0bc4f
mister chalmers <chalmers@chalmers.cards>
2022-01-03 16:30:16 -0500

    writes current occupancy value to occupancy counter and gage in setup

--- src/main.cpp
+++ src/main.cpp
@@ -276,7 +276,13 @@ void setup()
       delay(4000);
 
       occupancy_request(client, occupancy, "pull");
+
+      // pull latest occupancy capacity numbers from remote and set them to the display
       gslc_SetPageCur(&m_gui, E_PG_MAIN);
+      char string_to_write[MAX_STR];
+      snprintf(string_to_write, MAX_STR, "%u", occupancy);
+      gslc_ElemSetTxtStr(&m_gui, m_pElemVal2, string_to_write);
+      int gauge_pos = map(occupancy, 0, capacity, 0, 100);
       gslc_Update(&m_gui);
     }
 
