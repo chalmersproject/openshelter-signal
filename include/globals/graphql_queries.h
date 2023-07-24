e2e61769afd44ae0b3aca3798da23ac42e2f95cd
Zach Donsky <zach@cortex-design.com>
2023-07-24 16:04:11 -0400

    updated graphql queries with new queries. Uses cpp multi-string literals now.

--- include/globals/graphql_queries.h
+++ include/globals/graphql_queries.h
@@ -2,26 +2,28 @@
 #define GRAPHQL_H
 
 // Attempting to do a multi-line variable declaration: HOWTO?
-const char *PUSH = "               \
-mutation CreateSignalMeasurement(  \
-  $signalId: ID!                   \
-  $secretKey: String!              \
-  $value: Int!                     \
-  ){                               \
-  createShelterMeasurement(        \
-    input: {                       \
-      signalId: $signalId,         \
-      secretKey: $signalSecret,    \
-      value: $measurement          \
-      }                            \
-  ) {                              \
-    shelterMeasurement {           \
-      id                           \
-      value                        \
-    }                              \
-    errors                         \
-  }                                \
-}";
+
+const char *PUSH = R"PUSH(
+mutation CreateSignalMeasurement(
+  $signalId: ID!
+  $secretKey: String!
+  $value: Int!
+  ){
+  createShelterMeasurement(
+    input: {
+      signalId: $signalId,       
+      secretKey: $signalSecret,    
+      value: $measurement 
+    }
+  ) {
+    shelterMeasurement {
+      id
+      value
+    }
+    errors
+  }
+} 
+)PUSH";
 
 const char *PULL = R"PULL(
 query CheckSignalMeasurement(
@@ -34,26 +36,5 @@ query CheckSignalMeasurement(
   }
 }
 )PULL";
-
-const char *PULL = "               \
-query CheckSignalMeasurement(      \
-  $signalId: ID!                   \
-) {                                \
-    signal(id: $signalId)  {       \
-      name                         \
-      measurements (limit:1){      \
-        occupancy                  \
-        {                          \
-          spots                    \
-          beds                     \
-        }                          \
-        capacity                   \
-        {                          \
-          spots                    \
-          beds                     \
-        }                          \
-      }                            \
-    }                              \
-}";
 
 #endif
